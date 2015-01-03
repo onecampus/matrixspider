0efc72884e477ded80934c286eacb1c74e7ee630
flowerwrong <sysuyangkang@gmail.com>
2015-01-03 16:46:48 +0800

    Add get single post with id.

--- src/main/java/com/onecampus/controller/PostController.java
+++ src/main/java/com/onecampus/controller/PostController.java
@@ -2,24 +2,39 @@ package com.onecampus.controller;
 
 import java.util.List;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.onecampus.domain.Post;
 import com.onecampus.util.HibernateUtil;
 
 @RestController
+@RequestMapping("/posts")
 public class PostController {
-	@RequestMapping("/posts")
+	@RequestMapping(method=RequestMethod.GET)
     public List<Post> posts() {
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		session.beginTransaction();
 		@SuppressWarnings("unchecked")
 		List<Post> posts = session.createQuery("from Post").list();
-		System.out.print("hehe");
 		session.getTransaction().commit();
 		session.close();
 		return posts;
     }
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public Post post(@PathVariable("id") long id) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		Query query = session.createQuery("from Post where id = :id");
+		query.setParameter("id", id);
+		Post post = (Post) query.uniqueResult();
+		session.getTransaction().commit();
+		session.close();
+		return post;
+	}
 }
--- src/main/java/com/onecampus/service/PostServiceImpl.java
+++ src/main/java/com/onecampus/service/PostServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import com.onecampus.dao.PostDaoImpl;
 import com.onecampus.domain.Post;
 
 @Service
