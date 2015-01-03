08b38cf89f146e59a5a19ae13d1cf77bf867afbd
flowerwrong <sysuyangkang@gmail.com>
2015-01-03 13:52:42 +0800

    Add hibernate ok.

--- src/main/java/com/onecampus/domain/Post.java
+++ src/main/java/com/onecampus/domain/Post.java
@@ -2,9 +2,47 @@ package com.onecampus.domain;
 
 import java.util.Date;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="posts")
 public class Post {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
 	private long id;
+	
+	@Column(name="title")
 	private String title;
+	
+	@Column(name="content")
 	private String content;
+	
+	@Column(name="date")
 	private Date date;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
 }
--- src/main/java/com/onecampus/service/Demo.java
+++ src/main/java/com/onecampus/service/Demo.java
@@ -1,16 +1,30 @@
 package com.onecampus.service;
 
+import java.util.Date;
+
+import com.onecampus.domain.Post;
+import com.onecampus.util.HibernateUtil;
 import com.onecampus.util.SougouUtil;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
 
 public class Demo {
 	private static final Logger logger = LogManager.getLogger("Demo");
 
 	public static void main(String[] args) {
 		logger.warn("Hello, World!");
-		String openid = SougouUtil.getOpenid("36氪", "wow36kr");
-    	System.out.println(openid);
+		Post p = new Post();
+		p.setId(1);
+		p.setDate(new Date());
+		p.setTitle("title");
+		p.setContent("content");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		session.save(p);
+		session.getTransaction().commit();
+		session.close();
+		System.err.println("Done");
 	}
 }
